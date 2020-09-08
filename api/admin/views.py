2e07941855af72c994d04af454128788250aaa42
Wilson Marques <wilson.wmarques@gmail.com>
2020-09-08 18:17:59 +0100

    add readme

--- api/admin/views.py
+++ api/admin/views.py
@@ -21,20 +21,21 @@ def get_all_users(current_user):
 
     output = []
     for user in users:
-
         user_data = {}
         user_data['public_id'] = user.public_id
         user_data['name'] = user.name
         user_data['password'] = user.password
         user_data['admin'] = user.admin
         output.append(user_data)
-
     return jsonify({'users': output})
 
 
 @app.route('/user/<public_id>', methods=['GET'])
 @token_requiered
 def get_one_user(current_user ,public_id):
+
+    if not current_user.admin:
+        return jsonify({'message':'Cannot perform that function!'})
 
     user = User.query.filter_by(public_id=public_id).first()
 
@@ -53,6 +54,10 @@ def get_one_user(current_user ,public_id):
 @app.route('/user', methods=['POST'])
 @token_requiered
 def create_user(current_user):
+
+    if not current_user.admin:
+        return jsonify({'message':'Cannot perform that function!'})
+
     data = request.get_json()
 
     hashed_pass = generate_password_hash(data['password'], method='sha256')
@@ -71,14 +76,16 @@ def create_user(current_user):
 @app.route('/user/<public_id>', methods=['PUT'])
 @token_requiered
 def promote_user(current_user, public_id):
+
+    if not current_user.admin:
+        return jsonify({'message':'Cannot perform that function!'})
+
     user = User.query.filter_by(public_id=public_id).first()
 
     if not user:
         return jsonify({'message':'No user found!'})
-
     if user.admin:
         return jsonify({'message':'user is admin!'})
-
     user.admin = True
     db.session.commit()
 
@@ -88,6 +95,9 @@ def promote_user(current_user, public_id):
 @app.route('/user/<public_id>', methods=['DELETE'])
 @token_requiered
 def delete_user(current_user, public_id):
+
+    if not current_user.admin:
+        return jsonify({'message':'Cannot perform that function!'})
 
     user = User.query.filter_by(public_id=public_id).first()
 
---
+++ readme.md
@@ -0,0 +1,11 @@
+# RestFULL api with flask
+### For learning purposes
+<br>
+
+- Manage login withe JWT ( Json Web Tokens )
+- manage user privileges as permissions
+- Endpoints GET POST PUT DELETE ...
+- CRUD SQL database
+
+#### Backend to manage to do's and tasks!
+
